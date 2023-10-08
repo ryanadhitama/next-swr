bd5b3b522dd8f51fac8fe6e20201a84fc8dd5f04
ryanadhitama <ryanadhitama2@gmail.com>
2023-10-08 14:38:51 +0800

    integrate swr axios

---
+++ src/libs/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import config from '@/config';
+
+const instance = axios.create({
+  baseURL: config.API_HOST,
+  timeout: 40000,
+  headers: {
+    Accept: 'application/json, text/plain, */*',
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+});
+
+export const fetcher = (url) => {
+  return instance.get(url).then((res) => {
+    if (!res.data) {
+      throw Error(res.data.message);
+    }
+
+    return res.data;
+  });
+};
+
+export default instance;
